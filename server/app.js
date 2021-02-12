d4da69778bc27cedfaa45eb05b98513124d9fa7b
Phaneendra B <phani.brahma@gmail.com>
2021-02-11 22:23:06 -0800

    Added database config and tested connection

--- server/app.js
+++ server/app.js
@@ -6,11 +6,19 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+const uri = "mongodb://localhost:27017/blog";
+mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once("open", function () {
+  console.log("Connection success to mongodb server");
+});
+
 app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.json({ error: err });
 });
 
 app.listen(3000, () => {
-  console.log("Server started.");
+  console.log("Server started at 3000 port");
 });
