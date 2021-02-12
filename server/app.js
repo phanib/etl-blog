faf8a047b5141556af369443e8f5ed363b2bcd5f
Phaneendra B <phani.brahma@gmail.com>
2021-02-11 23:08:35 -0800

    Tying all the dots together. Working commit

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const passport = require("passport");
 const bodyParser = require("body-parser");
 const app = express();
+const UserModel = require("./models");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -10,10 +11,21 @@ const uri = "mongodb://localhost:27017/blog";
 mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
 const connection = mongoose.connection;
 
+require("./config/auth");
+
+const routes = require("./routes");
+const protectedRoutes = require("./routes/protected");
+
 connection.once("open", function () {
   console.log("Connection success to mongodb server");
 });
 
+app.use("/", routes);
+app.use(
+  "/user",
+  passport.authenticate("jwt", { session: false }),
+  protectedRoutes
+);
 app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.json({ error: err });
