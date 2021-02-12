ea15a9e551b404e61ec122645464b5ce23e00a83
Phaneendra B <phani.brahma@gmail.com>
2021-02-11 22:59:17 -0800

    Added login strategy to the passport

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,7 +1,8 @@
 const passport = require("passport");
 const localStrategy = require("passport-local").Strategy;
 const UserModel = require("../models");
-
+const JWTstrategy = require("passport-jwt").Strategy;
+const ExtractJWT = require("passport-jwt").ExtractJwt;
 /**
  * Passport middleware config for signup.
  * email and password are required fields
@@ -19,6 +20,57 @@ passport.use(
         const user = await UserModel.create({ email, password });
 
         return done(null, user);
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
+/**
+ * Configure middleware to valid the login of the user
+ */
+passport.use(
+  "login",
+  new localStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    async (email, password, done) => {
+      try {
+        const user = await UserModel.findOne({ email });
+
+        if (!user) {
+          return done(null, false, { message: "User not valid" });
+        }
+
+        const validate = await user.isValidPassword(password);
+
+        if (!validate) {
+          return done(null, false, { message: "Password is wrong" });
+        }
+
+        return done(null, user, { message: "Successfully logged" });
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
+);
+
+/**
+ *
+ */
+passport.use(
+  // TODO: Move the token to runtime environment
+  new JWTstrategy(
+    {
+      secretOrKey: "O0CkEjyolV29kN1Ljb7E2R4rnD",
+      jwtFromRequest: ExtractJWT.fromUrlQueryParameter("secret_token"),
+    },
+    async (token, done) => {
+      try {
+        return done(null, token.user);
       } catch (error) {
         done(error);
       }
