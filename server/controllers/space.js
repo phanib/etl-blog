33c62024f75217f7fb8f78744edaef26b54cd660
Phaneendra B <phani.brahma@gmail.com>
2021-02-12 15:18:03 -0800

    Space controller added

---
+++ server/controllers/space.js
@@ -0,0 +1,13 @@
+var Space = require("../models/space");
+
+exports.listSpaces = function (req, res) {
+  res.send("TBI");
+};
+
+exports.detailSpace = function (req, res) {
+  res.send("TBI" + req.params.id);
+};
+
+exports.createSpace = function (req, res) {
+  res.send("TBI");
+};
